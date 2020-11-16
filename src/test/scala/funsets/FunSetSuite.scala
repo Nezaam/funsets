3a7ae77e2da9b6b843ed7227f4bb89532ef73b20
Ilgiz Krymov <ilgiz.krymov@fv.dev>
2020-11-16 14:03:13 +0500

    Add tests

--- src/test/scala/funsets/FunSetSuite.scala
+++ src/test/scala/funsets/FunSetSuite.scala
@@ -35,19 +35,23 @@ class FunSetSuite {
    */
 
   trait TestSets {
-    val s1 = singletonSet(1)
-    val s2 = singletonSet(2)
-    val s3 = singletonSet(3)
+    val p1 = 1
+    val p2 = 200
+    val p3 = 17
+
+    val s1 = singletonSet(p1)
+    val s2 = singletonSet(p2)
+    val s3 = singletonSet(p3)
+    val s1Unions2Unions3 = union(s1, union(s2, s3))
+
+    def propEvenNumber(x: Int): Boolean = x % 2 == 0
+    def propOddNumber(x: Int): Boolean = !propEvenNumber(x)
+    def propConvertOddNumberToEvenNumber(x: Int): Int =
+      if(x % 2 == 0) x
+      else x + 1
   }
 
-  /**
-   * This test is currently disabled (by using @Ignore) because the method
-   * "singletonSet" is not yet implemented and the test would fail.
-   *
-   * Once you finish your implementation of "singletonSet", remvoe the
-   * @Ignore annotation.
-   */
-  @Ignore("not ready yet") @Test def `singleton set one contains one`: Unit = {
+  @Test def `singleton set one contains one`: Unit = {
 
     /**
      * We create a new instance of the "TestSets" trait, this gives us access
@@ -65,12 +69,73 @@ class FunSetSuite {
   @Test def `union contains all elements of each set`: Unit = {
     new TestSets {
       val s = union(s1, s2)
-      assert(contains(s, 1), "Union 1")
-      assert(contains(s, 2), "Union 2")
-      assert(!contains(s, 3), "Union 3")
+      assert(contains(s, p1), "Union 1")
+      assert(contains(s, p2), "Union 2")
+      assert(!contains(s, p3), "Union 3")
     }
   }
 
+  @Test def `intersect of s2 and s3 should not contain them`: Unit = {
+    new TestSets {
+      val s = intersect(s2, s3)
+      assert(!contains(s, p2), "Intersection 1")
+      assert(!contains(s, p3), "Intersection 2")
+    }
+  }
+
+  @Test def `intersect s1Unions2Unions3 and s3 should contain only s3`: Unit = {
+    new TestSets {
+      val s = intersect(s1Unions2Unions3, s3)
+      assert(!contains(s, p1), "Intersection 1")
+      assert(!contains(s, p2), "Intersection 2")
+      assert(contains(s, p3), "Intersection 3")
+    }
+  }
+
+  @Test def `diff of s1Unions2Unions3 and s3 should contain s1 and s2`: Unit = {
+    new TestSets {
+      val s = diff(s1Unions2Unions3, s3)
+      assert(contains(s, p1), "Diff 1")
+      assert(contains(s, p2), "Diff 2")
+      assert(!contains(s, p3), "Diff 3")
+    }
+  }
+
+  @Test def `filter s1Unions2Unions3 to get even number`: Unit = {
+    new TestSets {
+      val s = filter(s1Unions2Unions3, propEvenNumber)
+      assert(!contains(s, p1), "Filter 1")
+      assert(contains(s, p2), "Filter 2")
+      assert(!contains(s, p3), "Filter 3")
+    }
+  }
+
+  @Test def `forall test s1Unions2Unions3 properties?`: Unit = {
+    new TestSets {
+      assert(forall(s1Unions2Unions3, (x: Int) => x > 0), "forall > 0")
+      assert(!forall(s1Unions2Unions3, propEvenNumber), "forall even number")
+      assert(!forall(s1Unions2Unions3, propOddNumber), "forall odd number?")
+    }
+  }
+
+  @Test def `exists test s1Unions2Unions3 properties?`: Unit = {
+    new TestSets {
+      assert(exists(s1Unions2Unions3, (x: Int) => x > 100), "exists > 100")
+      assert(exists(s1Unions2Unions3, propEvenNumber), "exists even number")
+      assert(exists(s1Unions2Unions3, propOddNumber), "exists odd number?")
+      assert(exists(s1Unions2Unions3, propOddNumber)
+        || exists(s1Unions2Unions3, propEvenNumber), "exists number?")
+    }
+  }
+
+  @Test def `map test properties`: Unit = {
+    new TestSets {
+      val s = map(s1Unions2Unions3, propConvertOddNumberToEvenNumber)
+      printSet(s)
+      assert(!forall(s1Unions2Unions3, propEvenNumber), "forall s1Unions2Unions3 not even number")
+      assert(forall(s, propEvenNumber), "forall is even number")
+    }
+  }
 
 
   @Rule def individualTestTimeout = new org.junit.rules.Timeout(10 * 1000)
